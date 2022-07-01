bf42307ac01718c2ebbdd0b6fe5830f9970757ec
Sergio Andres Escobar Ramos <sescobarr@choucairtesting.com>
2022-07-01 12:40:20 -0500

    Optimización en la interfaz RegisterThreePage mapear algunos elementos por su name y el cambio del correo

--- src/main/java/co/com/choucair/reto_Utest/userinterface/RegisterThreePage.java
+++ src/main/java/co/com/choucair/reto_Utest/userinterface/RegisterThreePage.java
@@ -11,16 +11,16 @@ import static net.serenitybdd.core.annotations.findby.By.*;
 public class RegisterThreePage extends PageObject {
 
     public static final Target COMPUTER = Target.the("Select your computer")
-            .located(xpath("//div[@name='osId']"));
+            .located(name("osId"));
 
     public static final Target VERSION = Target.the("Select your version computer")
-            .located(xpath("//div[@name='osVersionId']"));
+            .located(name("osVersionId"));
 
     public static final Target LANGUAGE = Target.the("Select your language")
-            .located(xpath("//div[@name='osLanguageId']"));
+            .located(name("osLanguageId"));
 
     public static final Target MOBILE_DEVICE = Target.the("Select your mobile")
-            .located(xpath("//div[@name='handsetMakerId']"));
+            .located(name("handsetMakerId"));
 
     public static final Target MODEL = Target.the("Select your model of mobile")
             .located(name("handsetModelId"));
@@ -28,7 +28,7 @@ public class RegisterThreePage extends PageObject {
             .located(xpath("//input[@placeholder='Select a Model']"));
 
     public static final Target OPERATING_SYSTEM = Target.the("where we select the mobile operating system")
-            .located(xpath("//div[@name='handsetOSId']"));
+            .located(name("handsetOSId"));
 
     public static final Target LIST_ELEMENT_SELECTION = Target.the("Select item list")
             .locatedBy("//div[text()='{0}']");
