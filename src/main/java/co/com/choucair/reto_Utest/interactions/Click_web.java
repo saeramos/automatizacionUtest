f33d953d160f66eca5c0333438dc24ff2cde40a3
Sergio Andres Escobar Ramos <sescobarr@choucairtesting.com>
2022-06-30 22:20:45 -0500

    Agregando interaccion Click_web para la interfaz de registro de devices

---
+++ src/main/java/co/com/choucair/reto_Utest/interactions/Click_web.java
@@ -0,0 +1,30 @@
+package co.com.choucair.reto_Utest.interactions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.Tasks;
+import org.openqa.selenium.JavascriptExecutor;
+import net.serenitybdd.screenplay.targets.Target;
+
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
+
+public class Click_web  implements Performable {
+
+        Target target;
+
+        public Click_web(Target target) {
+            this.target = target;
+        }
+
+        @Override
+        public <T extends Actor> void performAs(T actor) {
+            JavascriptExecutor exe = (JavascriptExecutor)getDriver();
+            exe.executeScript("document.evaluate(arguments[0], document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();",target.getCssOrXPathSelector());
+        }
+
+        public static Click_web onXpath(Target target){
+            return Tasks.instrumented(Click_web.class, target);
+        }
+
+
+}
